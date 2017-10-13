039bf4bdd05ceb292134fa42f60ff913ba50d19b
Adam Martell <Adam@devcodecamp.com>
2017-10-13 13:48:31 -0500

    initial

---
+++ housing.js
@@ -0,0 +1,13 @@
+[
+    {
+    
+        "rentOrOwn" : "own",
+        "yearsOfOwnership" : 0,
+        "isOccupant" : true,
+        "50MedianIncome" : [ 25700, 29350, 33000, 36650,39600],
+        "homeType" : ["singleFamily", "duplex"]
+    },
+    {
+    
+    }
+]
